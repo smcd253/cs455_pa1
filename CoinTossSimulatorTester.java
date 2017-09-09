31fa24e668de06f5107600e52fb5ccb508edcc99
Spencer McDonough <shmcdono@usc.edu>
2017-09-09 10:56:38 -0700

    First commit

---
+++ CoinTossSimulatorTester.java
@@ -0,0 +1,14 @@
+//CoinTossSimulatorTester.java
+
+public class CoinTossSimulatorTester
+{
+    public static void main(String[] args)
+    {
+        CoinTossSimulator sim = new CoinTossSimulator();
+
+        // test 0
+        System.out.print("Number of Trials [exp: 0]: ");
+        System.out.println(sim.getNumTrials());
+        System.out.println();
+    }
+}
